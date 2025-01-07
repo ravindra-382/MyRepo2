ed7aae08006de724f16e48957c17966b1b7015a4
ravindra-382 <ravindra.gate382@gmail.com>
2025-01-07 12:09:33 +0530

    Added FirstProject1 into MyRepo2

---
+++ FirstProject1/src/axis/loans/goldloans/FirstClass.java
@@ -0,0 +1,12 @@
+package axis.loans.goldloans;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi000");
+	}
+
+
+	
+}
